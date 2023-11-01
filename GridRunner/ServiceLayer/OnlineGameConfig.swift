dcca40a4ba155db1f87d0907c2eed1aed81cdfb6
Imran Hajiyev <haciyev008@gmail.com>
2023-11-01 17:43:07 +0400

    refactor: removed unused update method from online game config

--- GridRunner/ServiceLayer/OnlineGameConfig.swift
+++ GridRunner/ServiceLayer/OnlineGameConfig.swift
@@ -21,16 +21,4 @@ class OnlineGameConfig: ResponseParser, Decodable, CustomStringConvertible, Conf
     var description: String {
         "GameConfig description. Current turn is \(turn)'s. Runner is: \(runner ?? "X"). Seeker is: \(seeker ?? "X"). Opponent is: \(opponent). Number of moves left for Runner: \(runnerMovesLeft). Number of moves left for Seeker: \(seekerMovesLeft). Type: \(type). \(grid). \(turnHistory)."
     }
-    
-    func update(with config: Configurable) {
-        self.grid = config.grid
-        self.runner = config.runner
-        self.seeker = config.seeker
-        self.opponent = config.opponent
-        self.runnerMovesLeft = config.runnerMovesLeft
-        self.seekerMovesLeft = config.seekerMovesLeft
-        self.turn = config.turn
-        self.turnHistory = config.turnHistory
-        self.type = config.type
-    }
 }
